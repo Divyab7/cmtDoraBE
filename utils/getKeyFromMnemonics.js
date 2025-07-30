8ab9b2b600533616ac665efa7cfc0732feb8fdfb
Divyab7 <bandadivya61@gmail.com>
2025-07-30 21:44:53 +0530

    Update getKeyFromMnemonics.js

--- utils/getKeyFromMnemonics.js
+++ utils/getKeyFromMnemonics.js
@@ -2,7 +2,7 @@ const { Mnemonic } = require("@hashgraph/sdk");
 
 async function getKeysFromMnemonic() {
     // Replace with your 24-word mnemonic
-    const phrase = "weird layer rule sniff police west month figure exit solve ball viable vocal achieve pitch myth answer liar safe ethics shine advance knock depart"; 
+    const phrase = ""; 
     
     // Load mnemonic
     const mnemonic = await Mnemonic.fromString(phrase);
